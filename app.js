5f81eba43e90a2d62a187c4a8316dd00e5dbdb0e
project159085 <project159085@gmail.com>
2025-10-29 15:51:06 +0530

    Basic Setup

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
+const path = require('path');
+const methodOverride = require('method-override');
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/Hotel_Booking";
+
+main()
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to this Webpage");
+});
+
+app.get("/listings", async (req, res) => {
+    const allListings =  await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log("Listing Deleted");
+    res.redirect("/listings");
+});
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1000,
+//         location: "Goa",
+//         country: "India"
+//     });
+    
+//     await sampleListing.save();
+//     console.log("Sample listing saved to database");
+//     res.send("Sample listing created and saved to database");
+// });
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage"
+    },
+    url: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1761405378261-ff90ecf72acf?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600",
+        set: (v) => v ==="" ? "https://images.unsplash.com/photo-1761405378261-ff90ecf72acf?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600" : v
+    }
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
