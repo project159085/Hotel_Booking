29c90e854274cc3fe6a475fa973356cb90561fa3
project159085 <project159085@gmail.com>
2025-11-04 14:35:39 +0530

    simple_validation

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Hotel_Booking";
@@ -23,7 +25,7 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,59 +34,82 @@ app.get("/", (req, res) => {
     res.send("Welcome to this Webpage");
 });
 
-app.get("/listings", async (req, res) => {
-    const allListings =  await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-});
+// index route
+app.get("/listings", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
 
+// new route
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 
-app.get("/listings/:id", async (req, res) => {
-    let {id} = req.params;
+//show route
+app.get("/listings/:id", wrapAsync (async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-});
+    res.render("listings/show.ejs", { listing });
+}));
 
-app.post("/listings", async (req, res) => {
+// create route
+app.post("/listings", wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send valid listing data");
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
-app.get("/listings/:id/edit", async (req, res) => {
-    let {id} = req.params;
+// edit route
+app.get("/listings/:id/edit", wrapAsync (async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-});
+    res.render("listings/edit.ejs", { listing });
+}));
 
-app.put("/listings/:id", async (req, res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+// update route
+app.put("/listings/:id", wrapAsync (async (req, res) => {
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send valid listing data");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-});
+}));
 
-app.delete("/listings/:id", async (req, res) => {
-    let {id} = req.params;
+// delete route
+app.delete("/listings/:id", wrapAsync (async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log("Listing Deleted");
     res.redirect("/listings");
-});
+}));
 
-// app.get("/testListing", async (req, res) => {
+// app.get("/testListing", wrapAsync (async (req, res) => {
 //     let sampleListing = new Listing({
 //         title: "My New Villa",
 //         description: "By the beach",
 //         price: 1000,
 //         location: "Goa",
 //         country: "India"
-//     });
-    
+//     }));
+
 //     await sampleListing.save();
 //     console.log("Sample listing saved to database");
 //     res.send("Sample listing created and saved to database");
 // });
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+    let {statusCode = 500, message = "Something Went Wrong"} = err;
+    res.status(statusCode).render("error.ejs", { message });
+    // res.status(statusCode).send(message);
+});
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
