99cd7783891c281033f6753a9536c03b939543e8
project159085 <project159085@gmail.com>
2025-11-09 18:59:22 +0530

    Changed Image Url to Image Upload and Cloud Setup to store Image Files

--- routes/listing.js
+++ routes/listing.js
@@ -3,30 +3,30 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require('../models/listing.js');
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const upload = multer({ dest: "upload/" })
 
-//index route
-router.get("/", wrapAsync(listingController.index));
 
-// new route
+//Index route and Create route combined
+router.route("/")
+    .get(wrapAsync(listingController.index))
+    // .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    .post(upload.single("listing[image]"), (req, res) => {
+        res.send(req.file);
+    });
+
+// New route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//Show route
-router.get("/:id", wrapAsync(listingController.showListing));
+// Show route, Update route and Delete route combined
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
-//Create Route
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing));
-
-// edit route
+// Edit route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.editListing));
-
-//Update route
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing));
-
-
-//Delete route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,18 +7,18 @@ const { saveRedirectUrl } = require('../middleware.js');
 
 const usersController = require('../controllers/users.js');
 
-// signup form
-router.get('/signup', usersController.renderSignup);
+// Signup Form Combined
+router.route('/signup')
+    .get(usersController.renderSignup)
+    .post(wrapAsync(usersController.signup));
 
-router.post('/signup', wrapAsync(usersController.signup));
-
-// login form
-router.get('/login', usersController.renderLogin);
-
-router.post('/login', saveRedirectUrl, passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/login'
-}), wrapAsync(usersController.login));
+// Login Form Combined
+router.route('/login')
+    .get(usersController.renderLogin)
+    .post(saveRedirectUrl, passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login'
+    }), wrapAsync(usersController.login));
 
 // logout route
 router.get('/logout', usersController.logout);
